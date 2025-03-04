f6b9f5215d7233fa76cc851398b5106c2054be4f
Rajiv Dwivedi <rajivdwivedi110085@gmail.com>
2025-03-04 11:59:29 +0530

    update ui

--- src/components/pages/Project/index.tsx
+++ src/components/pages/Project/index.tsx
@@ -8,12 +8,19 @@ import {
   Link,
 } from "@mui/material";
 
-const project = {
+const project1 = {
   id: 1,
   title: "My Project",
   description: "A modern Expense Tracker built with Reactjs.",
-  image: "/Screenshot.png", // Replace with your image path
+  image: "/Screenshot1.png", // Replace with your image path
   link: "https://vocal-squirrel-e54598.netlify.app/",
+};
+const project2 = {
+  id: 2,
+  title: "My Project",
+  description: "A modern Multi User Chat Room built with Next js and Socket io",
+  image: "/Screenshot 2.png", // Replace with your image path
+  link: "https://chat-app-rajiv.netlify.app/",
 };
 const Project = () => {
   return (
@@ -27,40 +34,76 @@ const Project = () => {
         >
           My Project
         </Typography>
-        <Card
-          sx={{
-            maxWidth: 600,
-            mx: "auto",
-            transition: "0.3s",
-            "&:hover": { transform: "scale(1.05)", boxShadow: 6 },
-            backgroundColor: "transparent",
-          }}
-        >
-          <CardMedia component="img" height="300" image={project.image} />
-          <CardContent>
-            <Typography
-              variant="h5"
-              fontWeight="bold"
-              mb={1}
-              sx={{ color: "white" }}
-            >
-              {project.title}
-            </Typography>
-            <Typography variant="body1" mb={2} sx={{ color: "white" }}>
-              {project.description}
-            </Typography>
-            <Link
-              href={project.link}
-              target="_blank"
-              rel="noopener"
-              color="primary"
-              underline="hover"
-              sx={{ fontWeight: "bold" }}
-            >
-              View Project
-            </Link>
-          </CardContent>
-        </Card>
+        <Box sx={{ display: "flex" }}>
+          <Card
+            sx={{
+              maxWidth: 600,
+              mx: "auto",
+              transition: "0.3s",
+              "&:hover": { transform: "scale(1.05)", boxShadow: 6 },
+              backgroundColor: "transparent",
+            }}
+          >
+            <CardMedia component="img" height="300" image={project1.image} />
+            <CardContent>
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                mb={1}
+                sx={{ color: "white" }}
+              >
+                {project1.title}
+              </Typography>
+              <Typography variant="body1" mb={2} sx={{ color: "white" }}>
+                {project1.description}
+              </Typography>
+              <Link
+                href={project1.link}
+                target="_blank"
+                rel="noopener"
+                color="primary"
+                underline="hover"
+                sx={{ fontWeight: "bold" }}
+              >
+                View Project
+              </Link>
+            </CardContent>
+          </Card>
+          <Card
+            sx={{
+              maxWidth: 600,
+              mx: "auto",
+              transition: "0.3s",
+              "&:hover": { transform: "scale(1.05)", boxShadow: 6 },
+              backgroundColor: "transparent",
+            }}
+          >
+            <CardMedia component="img" height="300" image={project2.image} />
+            <CardContent>
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                mb={1}
+                sx={{ color: "white" }}
+              >
+                {project2.title}
+              </Typography>
+              <Typography variant="body1" mb={2} sx={{ color: "white" }}>
+                {project2.description}
+              </Typography>
+              <Link
+                href={project2.link}
+                target="_blank"
+                rel="noopener"
+                color="primary"
+                underline="hover"
+                sx={{ fontWeight: "bold" }}
+              >
+                View Project
+              </Link>
+            </CardContent>
+          </Card>
+        </Box>
       </Box>
     </>
   );
